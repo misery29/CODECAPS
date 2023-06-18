62a58ff30138b52ce6d0c7e934c92fd66b6b4d70
João Pedro <jaohds@gmail.com>
2023-06-17 21:14:40 -0300

    teste

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the hamburger button and navigation
+    var hamburgerButton = document.querySelector('.hamburger-button');
+    var hamburgerNav = document.querySelector('.hamburger-nav');
+  
+    // Add a click event listener to the hamburger button
+    hamburgerButton.addEventListener('click', function() {
+      // Toggle the 'active' class on the hamburger navigation
+      hamburgerNav.classList.toggle('active');
+    });
+  });
+
 document.addEventListener('click', e =>{
     const isDropdownButton = e.target.matches("[data-dropdown-button]")
     if(!isDropdownButton && e.target.closest("[data-dropdown]") != null)return
@@ -13,6 +25,31 @@ document.addEventListener('click', e =>{
         dropdown.classList.remove("active")
     })
 })
+
+document.querySelector('.search-icon-button').addEventListener('click', function() {
+    var searchInput = document.querySelector('.search-input');
+    var logoSmall = document.querySelector('.logo-small');
+    if (searchInput.classList.contains('active')) {
+      searchInput.classList.remove('active');
+      logoSmall.classList.remove('shrink');
+    } else {
+      searchInput.classList.add('active');
+      logoSmall.classList.add('shrink');
+    }
+  });
+
+  document.querySelector('.hamburger-menu hamrburger-button').addEventListener('click', function() {
+    var hamburgerNav = document.querySelector('.hamburger-nav');
+    if (hamburgerNav.classList.contains('active')) {
+      hamburgerNav.classList.remove('active');
+      console.log("Script is running!");
+
+    } else {
+      hamburgerNav.classList.add('active');
+      console.log("Script is running!");
+
+    }
+  });
 
 var counter = 1;
 setInterval(function(){
